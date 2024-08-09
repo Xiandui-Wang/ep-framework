b0a361e738ca1510f73c9f00be5526d54e02972d
wangxiandui <wangxiandui@meituan.com>
2024-08-09 14:54:54 +0800

    init

---
+++ src/main/java/com/lucifiere/epf/EpFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.lucifiere.epf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucifiere/epf/EpFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucifiere.epf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
